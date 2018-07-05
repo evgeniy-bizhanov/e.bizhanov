f5a84ee939c890d0d8515bf724430e17246fd91a
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-07-05 11:13:32 +0300

    Добавлены сущности пользователя

---
+++ e.bizhanov/App/Entity/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  e.bizhanov
+//
+//  Created by Евгений Бижанов on 05.07.2018.
+//  Copyright © 2018 Евгений Бижанов. All rights reserved.
+//
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
+
+struct User: Codable {
+    let id: Int
+    let login: String
+    let name: String
+    let lastname: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id_user"
+        case login = "user_login"
+        case name = "user_name"
+        case lastname = "user_lastname"
+    }
+}
