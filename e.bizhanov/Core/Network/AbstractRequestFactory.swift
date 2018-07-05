5ed7845025c2da476c3075de33c469485a4f3dcb
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-07-05 10:20:21 +0300

    Добавлена фабрика и роутер запросов

---
+++ e.bizhanov/Core/Network/AbstractRequestFactory.swift
@@ -0,0 +1,28 @@
+//
+//  AbstractRequestFactory.swift
+//  e.bizhanov
+//
+//  Created by Евгений Бижанов on 05.07.2018.
+//  Copyright © 2018 Евгений Бижанов. All rights reserved.
+//
+
+import Alamofire
+
+protocol AbstractRequestFactory {
+    var errorParser: ​AbstractErrorParser​ { get }
+    var sessionManager: SessionManager { get }
+    var queue: DispatchQueue? { get }
+    
+    @discardableResult
+    func request<T: Decodable>(request: URLRequestConvertible,
+                               completionHandler: @escaping (DataResponse<T>) -> Void) -> DataRequest
+}
+
+extension AbstractRequestFactory {
+    public func request<T: Decodable>(request: URLRequestConvertible,
+                                      completionHandler: @escaping (DataResponse<T>) -> Void) -> DataRequest {
+        return sessionManager
+            .request(request)
+            .responseCodable(errorParser: errorParser, queue: queue, completionHandler: completionHandler)
+    }
+}
